e2f75c60fa6a2c40fb082b7e4ae9ee88eb596506
Forthtilliath <forth@live.fr>
2023-12-06 16:58:31 +0100

    feat: add day 6 part 1

--- day06/main.ts
+++ day06/main.ts
@@ -1,12 +1,12 @@
 /// <reference path="main.d.ts"/>
 
-import { exec, readFile } from "../utils";
+import { exec, multiply, readFile } from "../utils";
 
 const DAY = 6;
 
 function main(): void {
   console.clear();
-  const input = readFile(DAY, "input", false);
+  const input = readFile(DAY, "input");
   if (!input.length) {
     console.error("Invalid input data");
     return;
@@ -16,11 +16,23 @@ function main(): void {
   exec("input2", () => input2(input));
 }
 
-function input1(input: string): number {
-  return 0;
+function input1(input: string[]): number {
+  const [times, distances] = input.map((n) => n.match(/\d+/g)!.map(Number));
+
+  const ways: number[] = [];
+
+  for (let i in times) {
+    const results = Array.from({ length: times[i] }, (_, ms) => {
+      return (times[i] - ms) * ms;
+    });
+
+    const winnableResults = results.filter((res) => res > distances[i]);
+    ways.push(winnableResults.length)
+  }
+  return multiply(ways);
 }
 
-function input2(input: string): number {
+function input2(input: string[]): number {
   return 0;
 }
 
