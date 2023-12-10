def5884711f5e1ec51861dda16622d9d1c0faa04
Forthtilliath <forth@live.fr>
2023-12-10 04:44:30 +0100

    test: add more tests

--- day01/main.test.ts
+++ day01/main.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from "vitest";
-import { input1, input2 } from "./main";
 import { readFile } from "../lib/readFile";
+import { concatFirstAndLast, convert, input1, input2 } from "./main";
 
 const DAY = "01";
 
@@ -10,6 +10,11 @@ describe("Day " + DAY, () => {
   const sample2 = readFile(DAY, "sample2", true);
 
   describe("Part 1", () => {
+    it("shound concat the first element with the last element", () => {
+      expect(concatFirstAndLast(["4", "9", "8", "7", "2"])).toBe(42);
+      expect(concatFirstAndLast(["1", "8", "2", "3", "4"])).toBe(14);
+      expect(concatFirstAndLast(["2", "7", "9", "8", "3"])).toBe(23);
+    });
     it("sample1 should return the good value", () => {
       expect(input1(sample1)).toBe(142);
     });
@@ -19,6 +24,11 @@ describe("Day " + DAY, () => {
   });
 
   describe("Part 2", () => {
+    it("shound convert the line", () => {
+      expect(convert("4nineeightseven2")).toEqual(["4", "9", "8", "7", "2"]);
+      expect(convert("zoneight234")).toEqual(["1", "8", "2", "3", "4"]);
+      expect(convert("2sevenineight3")).toEqual(["2", "7", "9", "8", "3"]);
+    });
     it("sample2 should return the good value", () => {
       expect(input2(sample2)).toBe(281);
     });
--- day01/main.ts
+++ day01/main.ts
@@ -5,13 +5,28 @@ export function input1(input: string[]): number {
 
   for (let line of input) {
     const numbers = line.replace(/\D+/g, "").split("");
-    sum += Number(numbers[0] + numbers[numbers.length - 1]);
+    sum += concatFirstAndLast(numbers);
   }
 
   return sum;
 }
 
 export function input2(input: string[]): number {
+  let sum = 0;
+
+  for (let line of input) {
+    let numbers: string[] = convert(line);
+    sum += concatFirstAndLast(numbers);
+  }
+
+  return sum;
+}
+
+export function concatFirstAndLast(arr: string[]): number {
+  return Number(arr[0] + arr[arr.length - 1]);
+}
+
+export function convert(line: string): string[] {
   const transforms = {
     one: "1",
     two: "2",
@@ -24,25 +39,18 @@ export function input2(input: string[]): number {
     nine: "9",
   };
 
-  let sum = 0;
+  let numbers: string[] = [];
 
-  for (let line of input) {
-    let numbers: string[] = [];
+  for (const [key, value] of Object.entries(transforms)) {
+    if (line.includes(key)) {
+      const indexes = line.allIndexOf(key);
+      indexes.forEach((index) => (numbers[index] = value));
+    }
+    if (line.includes(value)) {
+      const indexes = line.allIndexOf(value);
+      indexes.forEach((index) => (numbers[index] = value));
+    }
+  }
 
-    for (const [key, value] of Object.entries(transforms)) {
-      if (line.includes(key)) {
-        const indexes = line.allIndexOf(key);
-        indexes.forEach((index) => (numbers[index] = value));
-      }
-      if (line.includes(value)) {
-        const indexes = line.allIndexOf(value);
-        indexes.forEach((index) => (numbers[index] = value));
-      }
-    };
-
-    numbers = numbers.filter(String);
-    sum += Number(numbers[0] + numbers[numbers.length - 1]);
-  };
-
-  return sum;
+  return numbers.filter(String);
 }
