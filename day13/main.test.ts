a7ed85210952c80477d09e64e97cd2389be94679
Forthtilliath <forth@live.fr>
2023-12-13 17:15:05 +0100

    feat: add day 13 part 2 + tests

--- day13/main.test.ts
+++ day13/main.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { input1, input2 } from "./main";
+import { getDifferences, getReflectionLineIndex, input1, input2, transpose } from "./main";
 import { readFile } from "../lib/readFile";
 
 const DAY = "13";
@@ -8,13 +8,89 @@ describe("Day " + DAY, () => {
   const input = readFile(DAY, "input", true);
   const sample = readFile(DAY, "sample", true);
 
+  describe("getDifferences", () => {
+    it("should return 0 when both strings are empty", () => {
+      expect(getDifferences("", "")).toBe(0);
+    });
+
+    it("should return 0 when both strings are the same", () => {
+      expect(getDifferences("abc", "abc")).toBe(0);
+    });
+
+    it("should return the correct number of differences when strings have different characters", () => {
+      expect(getDifferences("abc", "def")).toBe(3);
+      expect(getDifferences("hello", "world")).toBe(4);
+      expect(getDifferences("123", "321")).toBe(2);
+    });
+
+    it("should throw an error when strings have different lengths", () => {
+      expect(() => {
+        getDifferences("abc", "abcd");
+      }).toThrow("abc and abcd are not the same length");
+    });
+  });
+
+  describe("transpose", () => {
+    it("should transpose a string array", () => {
+      // prettier-ignore
+      const arr = [
+        "#.##.",
+        "..#.#",
+        "##...",
+        "##..."
+      ];
+      // prettier-ignore
+      const expected = [
+        "#.##",
+        "..##",
+        "##..",
+        "#...",
+        ".#.."
+      ];
+      expect(transpose(arr)).toEqual(expected);
+    });
+  });
+
+  describe("getReflectionLineIndex", () => {
+    it("should return the correct reflection line index", () => {
+      // Testing for rows with no reflection line
+      expect(getReflectionLineIndex(["abc", "def", "ghi"])).toBe(-1);
+      expect(getReflectionLineIndex(["ghi", "abc", "jkl", "abc"])).toBe(-1);
+
+      // Testing for rows with reflection lines
+      expect(getReflectionLineIndex(["abc", "def", "def", "abc", "ghi"])).toBe(2);
+      expect(getReflectionLineIndex(["abc", "def", "jkl", "jkl", "def", "abc"])).toBe(3);
+
+      // Testing for rows with reflection lines and maxDiff value
+      expect(getReflectionLineIndex(["abc", "def", "abc", "def", "ghi"], 1)).toBe(-1);
+      expect(getReflectionLineIndex(["abc", "def", "abc", "def", "ghi"], 2)).toBe(-1);
+      const arr = [
+        "#..###.#.",
+        "...#...##",
+        "##...##..",
+        "###.###..",
+        "##...#.##",
+        "...###.##",
+        "...#.#.##",
+        "##...#.##",
+        "###.###..",
+        "##...##..",
+        "...#...##",
+        "#..###.#.",
+        "#..###.#.",
+      ];
+      expect(getReflectionLineIndex(arr, 0)).toBe(12);
+      expect(getReflectionLineIndex(arr, 1)).toBe(6);
+    });
+  });
+
   describe("Part 1", () => {
     it("sample should return the good value", () => {
       expect(input1(sample)).toBe(405);
     });
     it("input should return the good value", () => {
       expect(input1(input)).toBe(37381);
-    }); 
+    });
   });
 
   describe("Part 2", () => {
--- day13/main.ts
+++ day13/main.ts
@@ -4,64 +4,51 @@ import "../helpers/extends";
 import chalk from "chalk";
 
 export function input1(input: string[]): number {
-  let res = 0;
-  const patterns = input.split("");
-
-  for (let pattern of patterns) {
-    let reflectionIndex = getReflectionLineIndex(pattern);
-
-    if (reflectionIndex !== -1) {
-      res += 100 * reflectionIndex;
-      continue;
-    }
-
-    pattern = transpose(pattern);
-    reflectionIndex = getReflectionLineIndex(pattern);
-    res += reflectionIndex;
-  }
-  return res;
+  return run(input);
 }
 
 export function input2(input: string[]): number {
+  return run(input, 1);
+}
+
+export function run(input: string[], maxDiff = 0): number {
   let res = 0;
   const patterns = input.split("");
 
   for (let pattern of patterns) {
-    console.log(chalk.blue`${pattern.join("\n")}`);
-    let reflectionIndex = getReflectionLineIndex(pattern, 1);
+    let reflectionIndex = getReflectionLineIndex(pattern, maxDiff);
+    console.log({reflectionIndex})
 
     if (reflectionIndex !== -1) {
       res += 100 * reflectionIndex;
       continue;
     }
 
-    pattern = transpose(pattern);
-
-    reflectionIndex = getReflectionLineIndex(pattern, 1);
-    console.log(chalk.green`reflectionIndex found: ${reflectionIndex}`);
+    reflectionIndex = getReflectionLineIndex(transpose(pattern), maxDiff);
     res += reflectionIndex;
   }
   return res;
 }
 
-function getReflectionLineIndex(rows: string[], maxDiff = 0): number {
+/**
+ * Finds the index of the line in the given array of rows where reflection occurs.
+ * 
+ * @param {string[]} rows - The array of rows to search for reflection.
+ * @param {number} maxDiff - The maximum allowed difference between adjacent rows.
+ * @returns {number} - The index of the line where reflection occurs, or -1 if no reflection is found.
+ */
+export function getReflectionLineIndex(rows: string[], maxDiff: number = 0): number {
+  console.log(chalk.yellow`===========`)
+  console.log(rows)
   line: for (let i = 0; i < rows.length - 1; i++) {
     let diff = 0;
-    console.log(chalk.yellow`===================`);
 
     for (let top = i, bottom = i + 1; top >= 0 && bottom < rows.length; top--, bottom++) {
       diff += getDifferences(rows[top], rows[bottom]);
-      console.log({ diff });
+
       if (diff > maxDiff) {
-        // for (let c = 0; c < rows[top].length; c++) {
-        //   console.log({ctop: rows[top][c], cbottom: rows[bottom][c]})
-        // if (rows[top][c] === rows[bottom][c] && fixeLeft) {
-        //     fixeLeft--;
         diff = 0;
         continue line;
-        //   }
-        //   diff++;
-        // }
       }
     }
 
@@ -73,32 +60,31 @@ function getReflectionLineIndex(rows: string[], maxDiff = 0): number {
   return -1;
 }
 
-function transpose(arr: string[]): string[] {
-  return arr[0].split("").map((_, j) => arr.map((_, i) => arr[i][j]).join(""));
+export function transpose(arr: string[]): string[] {
+  const result: string[] = [];
+
+  for (let j = 0; j < arr[0].length; j++) {
+    let column = "";
+    for (let i = 0; i < arr.length; i++) {
+      column += arr[i][j];
+    }
+    result.push(column);
+  }
+
+  return result;
 }
 
-function getDifferences(a: string, b: string): number {
+export function getDifferences(a: string, b: string): number {
+  if (a.length !== b.length) {
+    throw new Error(`${a} and ${b} are not the same length`);
+  }
+  
   let diff = 0;
-  if (a.length !== b.length) throw new Error("Not the same length");
-  if (a === b) return 0;
-
-  for (let i = 0; i < a.length; i++) {
-    if (a.charAt(i) !== b.charAt(i)) {
+  for (let i = 0, len = a.length; i < len; i++) {
+    if (a[i] !== b[i]) {
       diff++;
     }
   }
+  
   return diff;
 }
-
-// function areEquals(a: string, b: string, differencesAccepted = 0): boolean {
-//   console.log({ a, b, differencesAccepted });
-//   if (a.length !== b.length) throw new Error("Not the same length");
-//   if (a === b) return true;
-
-//   for (let i = 0; i < a.length && differencesAccepted >= 0; i++) {
-//     if (a.charAt(i) !== b.charAt(i)) {
-//       differencesAccepted--;
-//     }
-//   }
-//   return differencesAccepted >= 0;
-// }
